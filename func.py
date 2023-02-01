f32cdeeec94d2c7dd48c248dfcd320acfca63dbf
ericowen <ejo.owen@icloud.com>
2023-02-01 12:12:27 -0500

    creates add button to gui and its functionality

--- func.py
+++ func.py
@@ -1,9 +1,11 @@
-def get_todos(path='todos.txt'):
+FILEPATH = 'todos.txt'
+
+def get_todos(path=FILEPATH):
     with open(path, 'r') as file:
         todos = file.readlines()
     return todos
 
 
-def write_todos(todos_arg, path='todos.txt'):
+def write_todos(todos_arg, path=FILEPATH):
     with open(path, 'w') as file:
         file.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -2,9 +2,21 @@ import func
 import PySimpleGUI as sg
 
 label = sg.Text("type in a todo")
-input_box = sg.InputText(tooltip='enter todo')
+input_box = sg.InputText(tooltip='enter todo', key='todo')
 add_button = sg.Button('add')
 
-window = sg.Window('my todo app', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('my todo app',
+                   layout=[[label], [input_box, add_button]])
+
+while True:
+    event, values = window.read()
+    match event:
+        case 'add':
+            todos = func.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            func.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
